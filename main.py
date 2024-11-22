57c65b5d6c64636a6b8ff6a54ab8b6942d8b90f0
Haxous742 <105858936+Haxous742@users.noreply.github.com>
2024-11-22 14:44:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import tkinter as tk
+from UI import *
+from CSV_Handler import CSV_Handler
+from Members import *
+from Houses import *
+from Devices import *
 
+root = tk.Tk()
+root.title("Home Management System")
+root.geometry("500x300")
+root.config(bg="#f5f5f5")
+
+DeviceObjs = Devices()
+Devices.Device_details = CSV_Handler.loadDevices()
+
+MemberObjs = Members()
+Members.Member_details = CSV_Handler.loadMembers()
+
+HouseObjs = Houses()
+Houses.House_details = CSV_Handler.loadHouses()
+
+GuiObj = GUI(root,MemberObjs,DeviceObjs,HouseObjs)
+
+GuiObj.login_page()
+
+root.mainloop()
